4da62a05bc13894f1a6754b514cfca6b6932349e
OBIDJ0N <olimovmuhammaddiyor5@gmail.com>
2024-02-01 10:22:16 +0500

    Uploaded all files

---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import Hero from './Hero';
+import PopularProducts from './PopularProducts';
+import SuperQuality from './SuperQuality';
+import Services from './Services';
+import SpecialOffers from './SpecialOffers';
+import CustomerReviewes from './CustomerReviewes';
+import Subscribe from './Subscribe';
+import Footer from './Footer';
+
+export {
+    Hero,
+    PopularProducts,
+    SuperQuality,
+    Services,
+    SpecialOffers,
+    CustomerReviewes,
+    Subscribe,
+    Footer
+}
